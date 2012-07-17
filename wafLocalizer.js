1833c4e0c5e19fbecb2bbaad160d6af1cee875b6
Thibaud Arguillere <thibarg.web@free.fr>
2012-07-17 10:10:42 +0200

    Add localizeString() and setOptions()

--- wafLocalizer.js
+++ wafLocalizer.js
@@ -1,11 +1,19 @@
 ﻿/*	wafLocalizer.js
 
-	Version 1.0, 2012-07-15
-
 	Utilities to localize Wakanda Application Framework widgets at runtime
 
 	(c) 4D SAS, author: Thibaud Arguillere
 	License: MIT. See the license at the end of this source code
+
+	--------------------------------------
+	Versions
+	--------------------------------------
+	1.0, 2012-07-15
+		First version
+	
+	1.1, 2012-07-16
+		Added localizeString()
+		Added setOptons()
 	
 	--------------------------------------
 	Usage
@@ -37,17 +45,30 @@
 	Instance methods
 	--------------------------------------
 		localizeWidgets()
+			As its name states. Should be called in the onLoad event
+		
+		localizeString(inString)
+			To be used with dynamic strings. An alert for example:
+			alert( theLocalizer.localizeString("Everything is gonna be ok.") );
+		
+		setOptions(inOptions)
+			inOptions is an obect. As of today (2012-07-16), the method should
+			not end with a 's' because there is only one option ;->
+				caseSensitive
+					Default value is true
+					Passing false => the key value is compared using lowerCase
+					and no accent
 */
 
 if(typeof WAFLocalizer === 'undefined') {
-		
+
 	(function _scope_localize() {
 		// Shared variables
-		var _version = '1.0';
+		var _version = '1.1';
 	
 		// Constructor accepts either 2 synchronized arrays, or the final, key/value array
 		function _WAFLocalizer(inSrceOrFinal, inLocalized) {
-			var i, max, _localizedValues = [], _ignoreSrceCase = false;
+			var i, max, _localizedValues = [], _localizedValuesLowerCase = [], _caseSensitive = true;
 			
 			// Build the key/value array
 			if(inSrceOrFinal && !inLocalized) {
@@ -60,16 +81,58 @@ if(typeof WAFLocalizer === 'undefined') {
 				throw new TypeError(inSrceOrFinal)
 			}
 			
+			
+		//--------------------------------------
+		// Internal utilities
+		//--------------------------------------
+			// _getLowerCaseNoAccents assumes inString really is a string
+			//--------------------------------------
+			function _getLowerCaseNoAccents(inString) {
+			//--------------------------------------
+				if(inString) {
+					return inString.toLowerCase().replace(/[àáâãäå]/g,"a")
+										.replace(/[éèêë]/g,"e")
+										.replace(/[ìíîï]/g,"i")
+										.replace(/[ðòóôõöø]/g,"o")
+										.replace(/[ùúûü]/g,"u")
+										.replace(/[ýýÿ]/g,"y");
+				}
+				
+				return '';
+			}
+			
+			//--------------------------------------
+			function _createLocalizedValuesLowerCase() {
+			//--------------------------------------
+				var theKey, theKeyLowerCase;
+				
+				_localizedValuesLowerCase = [];
+				for(theKey in _localizedValues) {
+					_localizedValuesLowerCase[ _getLowerCaseNoAccents(theKey) ] = _localizedValues[ theKey ];
+				}
+			}
+			
 			// Utility that returns the original value if no translation is found
 			//--------------------------------------
 			function _getLocalizedValue(inSrceValue) {
 			//--------------------------------------
-				var localized = inSrceValue;
+				var localized = '';
 
 				if (inSrceValue) {
 					localized = _localizedValues[inSrceValue];
 					if (!localized) {
-						localized = inSrceValue;
+						if(_caseSensitive) {
+							localized = inSrceValue;
+						} else {
+							// Create the lowerCase localized values if needed
+							if(_localizedValuesLowerCase.length === 0) {
+								_createLocalizedValuesLowerCase();
+							}
+							localized = _localizedValues[ _getLowerCaseNoAccents(inSrceValue) ];
+							if(!localized) {
+								localized = inSrceValue;
+							}
+						}
 					}
 				}
 
@@ -79,6 +142,13 @@ if(typeof WAFLocalizer === 'undefined') {
 		//--------------------------------------
 		//Instance methods
 		//--------------------------------------
+			//--------------------------------------
+			this.setOptions = function(inOptions) {
+			//--------------------------------------
+				if(inOptions) {
+					_caseSensitive = typeof inOptions.caseSensitive === 'boolean' ? inOptions.caseSensitive : _caseSensitive;
+				}
+			}
 			
 			//--------------------------------------
 			this.localizeWidgets = function() {
@@ -187,13 +257,20 @@ if(typeof WAFLocalizer === 'undefined') {
 				Object.keys(WAF.widgets).forEach(function(inID) {
 					// WAF.widgets is an object, wich contains also properties
 					// that are not widgets ("_length" for example)
-					// Just checking some properties of any widget
 					var widget = WAF.widgets[inID];
 					if (widget instanceof WAF.Widget) {
 						_doIt(widget);
 					}
 				});
+			} // localizeWidgets()
+
+			//--------------------------------------
+			this.localizeString = function(inString) {
+			//--------------------------------------
+				return _getLocalizedValue(inString);
 			}
+			
+			
 		} // function _WAFLocalizer
 	
 		//--------------------------------------
@@ -205,10 +282,36 @@ if(typeof WAFLocalizer === 'undefined') {
 		
 	
 		//--------------------------------------
-		// Finally, make the whole stuff avilable everywhere
+		// Finally, make the whole stuff available everywhere
 		//--------------------------------------
 		WAFLocalizer = _WAFLocalizer;
 		
 	})();
 	
 } // if(typeof wafLocalizer === 'undefined')
+
+/*	License: MIT (http://www.opensource.org/licenses/MIT)
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+	
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// --EOF
