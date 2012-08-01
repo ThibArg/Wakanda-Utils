de9d788bea79d7b5639131395f3c55e97eb16ecb
Thibaud Arguillere <thibarg.web@free.fr>
2012-08-01 10:57:52 +0200

    Added WU.enable()

--- wafUtils.js
+++ wafUtils.js
@@ -1,6 +1,4 @@
 ﻿/*	wafUtils
-
-	2012-06-03
 	
 	A set of utilities to handle Wakanda (http://wakanda.org) widgets. This code adds features
 	to the WAF (Wakanda Application Framework) client side.
@@ -15,6 +13,10 @@
 	Original source code by Thibaud Arguillere.
 	Source code under the MIT license (see at the end of this file).
 	
+	Last update
+		2012-08-01
+			Added WU.enable
+	
 	-------------------------------------------------------------------------------
 	Usage
 	-------------------------------------------------------------------------------
@@ -27,17 +29,29 @@
 	List of routines. _OPT_ means the parameter is optionnal
 	-------------------------------------------------------------------------------
 	* Utilities for all widgets
-		Extending the Widget API: The routines are added to the Widget prototype, so you
-		call them from the Widget API. For example:
-				$$('myGridId').WU_fadeOut();
-				$$('myButtonId').WU_fadeIn();
+		- Extending the Widget API: The routines are added to the Widget prototype, so you
+		  call them from the Widget API. For example:
+					$$('myGridId').WU_fadeOut();
+					$$('myButtonId').WU_fadeIn();
 
-		WU_fadeIn(_OPT_ inDuration, _OPT_ inCallback)
-		WU_fadeOut(_OPT_ inDuration, _OPT_ inCallback)
+				WU_fadeIn(_OPT_ inDuration, _OPT_ inCallback)
+				WU_fadeOut(_OPT_ inDuration, _OPT_ inCallback)
 
-		WU_clearTextFields()
-			Call setValue('') for every widget of kind TextField inside the caller.
-			Warning: This works only for the first level of the widget
+				WU_clearTextFields()
+					Call setValue('') for every widget of kind TextField inside the caller.
+					Warning: This works only for the first level of the widget
+		
+		- Funtions (not extending the class)
+			WU.enable(doEnable, anArray)
+				doEnable is a boolean
+				anArray contains an array of widgets, widget IDs or a mix.
+				
+				Example:
+					WU.enable(false, [$$('aWidget'), 'anotherWidget', 'yetAnotherOne']);
+				
+				The goal is to reduce the lines of code when you often need to enable/disable
+				several widgets.
+		
 
 
 	* Utilities for Datagrids
@@ -60,259 +74,147 @@
 if(typeof WU === 'undefined') {
 	WU = {};
 	
-	// --------------------------------------------
-	WU.getVersion = function() {
-	// --------------------------------------------
-		return "1.1";
-	}
-	
-	
-	function _buildLocalizedArray(inSrce, inDest, inoutResult) {
-		var i, max, ok = false;
-
-		if (inSrce && inDest && inSrce.length && inDest.length && inSrce.length === inDest.length) {
-			for (i = 0, max = inSrce.length; i < max; i++) {
-				inoutResult[ inSrce[i] ] = inDest[i];
-			}
-			ok = true;
-		}
-		return ok;
-	}
-
-	// --------------------------------------------
-	WU.localizeArray = function(inSrce, inDest, inoutResult) {
-	// --------------------------------------------
-		_buildLocalizedArray(inSrce, inDest, inoutResult);
-	}
-
-	// --------------------------------------------
-	WU.localizeWidgets = function(inSrce, inDest) {
+	// Scope_WU is just for debug symbol (reading Scope_WUB instead of "anonymous")
+	(function Scope_WU () {
+		//	===========================================
+		//	Widget
+		//	===========================================
 		// --------------------------------------------
-		var _localized = [],
-			i, max;
-
-		function _init() {
-			_localized = [];
-			return _buildLocalizedArray(inSrce, inDest, _localized);
-		}
-
-		// Utility that returns the original value if no translation is found
-		function _getLocalizedValue(inSrceValue) {
-			var localized = inSrceValue;
-
-			if (inSrceValue) {
-				localized = _localized[inSrceValue];
-				if (!localized) {
-					localized = inSrceValue;
-				}
-			}
-
-			return localized;
-		}
-
-		// Main routine, exhanging the values depending on the kind of widget
-		function _doIt(inWidget) {
-			var subWidgets, container, label, anArray, anObject;
-
-			if (inWidget) {
-				switch (inWidget.kind) {
-				case 'dataGrid':
-					inWidget.columns().forEach(function(inColumn) {
-						inColumn.title = _getLocalizedValue(inColumn.title);
-						inColumn.gridview._private.globals.headerContainerNode.cells[inColumn.columnNumber].title.html(inColumn.title);
-					});
-					break;
-
-				case 'autoForm':
-					// Hum, the autoForm class can be used for an autoForm or a queryForm
-					anArray = $('#' + inWidget.id + ' .waf-form-att-label-text');
-					if (anArray.length > 0) { // We have a true autoform
-						for (i = 0, max = anArray.length; i < max; i++) {
-							anObject = anArray[i];
-							anObject.innerText = _getLocalizedValue(anObject.innerText);
+		WU.enable = function(inEnable, inWidgets) {
+		// --------------------------------------------
+			if(inWidgets && inWidgets.length && 'forEach' in inWidgets) {
+				inWidgets.forEach(function(elt) {
+					var widg = null;
+					if(typeof elt === 'string') {
+						widg = $$(elt);
+					} else if('enable' in elt && 'disable' in elt) {
+						widg = elt;
+					}
+					
+					if(widg) {
+						if(inEnable) {
+							widg.enable();
+						} else {
+							widg.disable();
 						}
 					}
-					else {
-						anArray = $('#' + inWidget.id + ' .attribute-div');
-						if (anArray.length > 0) { // We have a queryform
-							for (i = 0, max = anArray.length; i < max; i++) {
-								anObject = anArray[i];
-								anObject.innerText = _getLocalizedValue(anObject.innerText);
-							}
-						}
-					}
-					break;
-
-				default:
-					subWidgets = null;
-					// Every menuItem of a menuBar is part of WAF.widgets => we don't
-					// need to handle them separatly with getChildren()
-					if (inWidget.kind !== 'menuBar') {
-						subWidgets = inWidget.getChildren();
-					}
-
-					if (subWidgets && subWidgets.length > 0) {
-						subWidgets.forEach(function(inSubWidget) {
-							_doIt(inSubWidget);
-						});
-					}
-					else {
-						switch (inWidget.kind) {
-						case 'button':
-						case 'label':
-						case 'richText':
-							inWidget.setValue(_getLocalizedValue(inWidget.getValue()));
-							break;
-
-						case 'menuItem':
-							debugger;
-							inWidget.domNode.innerText = _getLocalizedValue(inWidget.domNode.innerText);
-							break;
-
-						}
-
-						if (inWidget.getLabel) {
-							label = inWidget.getLabel();
-							if (label) {
-								label.setValue(_getLocalizedValue(label.getValue()));
-							}
-						}
-
-						if (inWidget.domNode && inWidget.domNode.placeholder) {
-							inWidget.domNode.placeholder = _getLocalizedValue(inWidget.domNode.placeholder);
-						}
-					}
-					break;
-				}
+				});
 			}
 		}
-
-		if (_init()) {
-			Object.keys(WAF.widgets).forEach(function(inID) {
-				// WAF.widgets is an object, wich contains also properties
-				// that are not widgets ("_length" for example)
-				// Just checking some properties of any widget
-				var widget = WAF.widgets[inID];
-				if (widget instanceof WAF.Widget) {
-					_doIt(widget);
+		
+		// Save microseconds by resolving WAF.Widget.prototype just once
+		var _WIDGET_PROTOTYPE = WAF.Widget.prototype;
+		// --------------------------------------------
+		_WIDGET_PROTOTYPE.WU_fadeOut = function(inDuration, inCallback) {
+		// --------------------------------------------
+			if (typeof inDuration === "undefined") {
+				inDuration = '';
+			}
+			
+			if (typeof inCallback !== "function") {
+				inCallback = null;
+			}
+			$("#" + this.id).fadeOut(inDuration, inCallback);
+		}
+		
+		// --------------------------------------------
+		_WIDGET_PROTOTYPE.WU_fadeIn = function(inDuration, inCallback) {
+		// --------------------------------------------
+			if (typeof inDuration === "undefined") {
+				inDuration = '';
+			}
+			
+			if (typeof inCallback !== "function") {
+				inCallback = null;
+			}
+			$("#" + this.id).fadeIn(inDuration, inCallback);
+		}
+		
+		// --------------------------------------------
+		_WIDGET_PROTOTYPE.WU_clearTextFields = function() {
+		// --------------------------------------------
+			this.getChildren().forEach(function(inWidget) {
+				if(inWidget && inWidget.kind === 'textField') {
+					inWidget.setValue('');
 				}
 			});
 		}
-	}
-	
-	//	===========================================
-	//	Widget
-	//	===========================================
-	// Save microseconds by resolving WAF.Widget.prototype just once
-	var _WIDGET_PROTOTYPE = WAF.Widget.prototype;
-	// --------------------------------------------
-	_WIDGET_PROTOTYPE.WU_fadeOut = function(inDuration, inCallback) {
-	// --------------------------------------------
-		if (typeof inDuration === "undefined") {
-			inDuration = '';
-		}
+
+		//	===========================================
+		//	Grid
+		//	===========================================
+		// Save microseconds by resolving WAF.classes.DataGrid.prototype just once
+		var _GRID_PROTOTYPE = WAF.classes.DataGrid.prototype;
 		
-		if (typeof inCallback !== "function") {
-			inCallback = null;
-		}
-		$("#" + this.id).fadeOut(inDuration, inCallback);
-	}
-	
-	// --------------------------------------------
-	_WIDGET_PROTOTYPE.WU_fadeIn = function(inDuration, inCallback) {
-	// --------------------------------------------
-		if (typeof inDuration === "undefined") {
-			inDuration = '';
-		}
+		// Hard coded class names. If WAF changes, just change the values according
+		// to the version (WAF.version)
+		var _CLASS_DG_BODY = ".waf-dataGrid-body";
+		var _CLASS_DG_FOOTER = ".waf-dataGrid-footer";
+		var _CLASS_DG_FOOTER_STATUS = ".waf-dataGrid-footer .waf-status-element";
+		var _CLASS_DG_FOOTER_BUTTONS = ".waf-dataGrid-footer .waf-toolbar";
+		var _CLASS_DG_FOOTER_INFOS = ".waf-dataGrid-footer .waf-status-center"
 		
-		if (typeof inCallback !== "function") {
-			inCallback = null;
-		}
-		$("#" + this.id).fadeIn(inDuration, inCallback);
-	}
-	
-	// --------------------------------------------
-	_WIDGET_PROTOTYPE.WU_clearTextFields = function() {
-	// --------------------------------------------
-		this.getChildren().forEach(function(inWidget) {
-			if(inWidget && inWidget.kind === 'textField') {
-				inWidget.setValue('');
+		// --------------------------------------------
+		_GRID_PROTOTYPE.WU_hideParts = function(inParts, inDoFadeOut) {
+		// --------------------------------------------
+			var jqueryPrefix = "#" + this.id + " ";
+			var fnToCall;
+
+			if(inDoFadeOut) {
+				fnToCall = $.fn.fadeOut;
+			} else {
+				fnToCall = $.fn.hide;
 			}
-		});
-	}
+						
+			if(inParts.body) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_BODY) );
+			}
+			
+			if(inParts.footer) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_STATUS) );
+			}
+			
+			if(inParts.buttons) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_BUTTONS) );
+			}
+			
+			if(inParts.footerInfos) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_INFOS) );
+			}
+		}
+		
 
-	//	===========================================
-	//	Grid
-	//	===========================================
-	// Save microseconds by resolving WAF.classes.DataGrid.prototype just once
-	var _GRID_PROTOTYPE = WAF.classes.DataGrid.prototype;
+		// --------------------------------------------
+		_GRID_PROTOTYPE.WU_showParts = function(inParts, inDoFadeIn) {
+		// --------------------------------------------
+			var jqueryPrefix = "#" + this.id + " ";
+			var fnToCall;
+
+			if(inDoFadeOut) {
+				fnToCall = $.fn.fadeIn;
+			} else {
+				fnToCall = $.fn.hide;
+			}
+			
+			if(inParts.body) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_BODY) );
+			}
+			
+			if(inParts.footer) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_STATUS) );
+			}
+			
+			if(inParts.buttons) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_BUTTONS) );
+			}
+			
+			if(inParts.footerInfos) {
+				fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_INFOS) );
+			}
+		}
 	
-	// Hard coded class names. If WAF changes, just change the values according
-	// to the version (WAF.version)
-	var _CLASS_DG_BODY = ".waf-dataGrid-body";
-	var _CLASS_DG_FOOTER = ".waf-dataGrid-footer";
-	var _CLASS_DG_FOOTER_STATUS = ".waf-dataGrid-footer .waf-status-element";
-	var _CLASS_DG_FOOTER_BUTTONS = ".waf-dataGrid-footer .waf-toolbar";
-	var _CLASS_DG_FOOTER_INFOS = ".waf-dataGrid-footer .waf-status-center"
+	})();
 	
-	// --------------------------------------------
-	_GRID_PROTOTYPE.WU_hideParts = function(inParts, inDoFadeOut) {
-	// --------------------------------------------
-		var jqueryPrefix = "#" + this.id + " ";
-		var fnToCall;
-
-		if(inDoFadeOut) {
-			fnToCall = $.fn.fadeOut;
-		} else {
-			fnToCall = $.fn.hide;
-		}
-					
-		if(inParts.body) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_BODY) );
-		}
-		
-		if(inParts.footer) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_STATUS) );
-		}
-		
-		if(inParts.buttons) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_BUTTONS) );
-		}
-		
-		if(inParts.footerInfos) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_INFOS) );
-		}
-	}
-	
-
-	// --------------------------------------------
-	_GRID_PROTOTYPE.WU_showParts = function(inParts, inDoFadeIn) {
-	// --------------------------------------------
-		var jqueryPrefix = "#" + this.id + " ";
-		var fnToCall;
-
-		if(inDoFadeOut) {
-			fnToCall = $.fn.fadeIn;
-		} else {
-			fnToCall = $.fn.hide;
-		}
-		
-		if(inParts.body) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_BODY) );
-		}
-		
-		if(inParts.footer) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_STATUS) );
-		}
-		
-		if(inParts.buttons) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_BUTTONS) );
-		}
-		
-		if(inParts.footerInfos) {
-			fnToCall.apply( $(jqueryPrefix + _CLASS_DG_FOOTER_INFOS) );
-		}
-	}	
 }
 
 
